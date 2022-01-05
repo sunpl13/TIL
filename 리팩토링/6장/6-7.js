b814e0474839923b9fbe029d3151cbec87d48eba
Jungsoo Kim <sunpl13@naver.com>
2022-01-05 22:19:17 +0900

    여러함수 클래스로 묶기

---
+++ 리팩토링/6장/6-7.js
@@ -0,0 +1,38 @@
+const station = {
+  name: "ZB1",
+  readings: [
+    { temp: 47, time: "2016-11-10 09:10" },
+    { temp: 53, time: "2016-11-10 09:20" },
+    { temp: 58, time: "2016-11-10 09:30" },
+    { temp: 53, time: "2016-11-10 09:40" },
+    { temp: 51, time: "2016-11-10 09:50" },
+  ],
+};
+
+function readingsOutsideRange(staion, min, max, range) {
+  return staion.readings.filter((r) => r.temp < min || r.temp > max);
+}
+
+alerts = readingsOutsideRange(
+  station,
+  operatingPlan.temperatureFloor,
+  operatingPlan.temperatureCeiling,
+  range
+);
+
+class NumberRange {
+  constructor(min, max) {
+    this._data = { min: min, max: max };
+  }
+  get min() {
+    return this._data.min;
+  }
+  get max() {
+    return this._data.max;
+  }
+}
+
+const range = new NumberRange(
+  operatingPlan.temperatureFloor,
+  operatingPlan.temperatureCeiling
+);
